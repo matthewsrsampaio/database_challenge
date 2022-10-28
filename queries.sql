f58a4bd56a88da06fe77f48a6496c50a4c4fa555
Matthews R. Sampaio <85257122+matthewsrsampaio@users.noreply.github.com>
2022-10-28 13:52:09 -0300

    pretty

--- queries.sql
+++ queries.sql
@@ -2,8 +2,10 @@
 
 -- Escreva uma consulta MySQL para:
 
--- 1. Escreva uma consulta MySQL para calcular o preço médio do m² dos imóveis com aumento de preço em 2016 que tenham uma área do metro quadrado > 200 (área construída ou usada).
-select cast(avg((pc.new_price/bua.built_area)) as decimal(10,2)) as Preço_médio_do_m²_dos_imóveis_com_aumento_em_2016_que_tem_área_maior_que_200m²
+-- 1. Escreva uma consulta MySQL para calcular o preço médio do m² dos imóveis com aumento de preço em 2016 que tenham uma área do metro quadrado > 200;
+-- (área construída ou usada).
+select
+cast(avg((pc.new_price/bua.built_area)) as decimal(10,2)) as Preço_médio_do_m²_dos_imóveis_com_aumento_em_2016_que_tem_área_maior_que_200m²
 from price_changes pc
 inner join built_used_area bua
 on (pc.listing_id = bua.listing_id)
