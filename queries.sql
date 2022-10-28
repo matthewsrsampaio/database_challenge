0227530bf409fa8ee3f8837e5f8414356e23f0e7
Matthews R. Sampaio <85257122+matthewsrsampaio@users.noreply.github.com>
2022-10-28 12:31:54 -0300

    pretty

--- queries.sql
+++ queries.sql
@@ -2,8 +2,6 @@
 
 -- 1. Escreva uma consulta MySQL para calcular o preço médio do m² dos imóveis com aumento de preço em 2016 que tenham uma área do metro quadrado > 200 (área construída ou usada).
 use desafio_atlantico;
-select * from price_changes;
-select * from built_used_area;
 
 select cast(avg((pc.new_price/bua.built_area)) as decimal(10,2)) as Preço_médio_do_m²_dos_imóveis_com_aumento_em_2016_que_tem_área_maior_que_200m²
 from price_changes pc
@@ -11,6 +9,7 @@ inner join built_used_area bua
 on (pc.listing_id = bua.listing_id)
 where pc.new_price > pc.old_price and bua.built_area > 200 and pc.change_date between '2016-01-01' and '2016-12-31';
 
+-- 2. Escreva uma consulta MySQL para:
 -- Contar o número de imóveis que tiveram aumento de preço em 2016 e o aumento médio do percentual de preço/m² desses imóveis;
 use desafio_atlantico;
 
